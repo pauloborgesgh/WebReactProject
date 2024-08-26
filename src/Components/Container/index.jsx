ce608763756f460fc68ca9c45faad910a0fe6ee3
pauloborgesgh <pauloborgeszambon@gmail.com>
2024-08-26 20:06:02 -0300

    ajust.menu

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -1,29 +1,20 @@
-
 import '../Container/container.css'
 
-
-function Container(){
-
-return(
+function Container() {
+  return (
     <section className='container'>
-    <div className='apresentacao'>
-  <p>
-   
-    <span>Unidos Somos Mais Forte</span> <br/>
- 
-  </p>
-
-
-
-  
-</div>
-{/* <figure>
-  <img className='img-home' src="../img/bem-vindo.png" alt="Imagem da Home" />
-</figure> */}
-</section>
-)
-
-
+      <div className='content'>
+        <div className='image-container'>
+          <img className='img-dengue' src="../img/img_menu.jpg" alt="Prevenção contra a Dengue" />
+        </div>
+        <div className='info-container'>
+          <p className='dengue-info'>
+            A dengue é um problema sério. Faça sua parte eliminando focos de água parada. Juntos, podemos combater a doença e proteger nossa comunidade!
+          </p>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Container
