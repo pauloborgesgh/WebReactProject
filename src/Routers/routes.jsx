a302258fb841e8310bd5c212c74dfca50ccd7490
pauloborgesgh <pauloborgeszambon@gmail.com>
2024-08-27 07:57:02 -0300

    router.config

--- src/Routers/routes.jsx
+++ src/Routers/routes.jsx
@@ -1,26 +1,4 @@
-// import { BrowserRouter, Routes, Route } from 'react-router-dom'
-// import Home from '../Pages/Home/index.jsx'
-// import Info from '../Pages/Info/index.jsx'
-// import Lista from '../Pages/Lista/index.jsx'
-// import Denuncia from '../Pages/Denuncia/index.jsx'
 
-
-// function AppRoutes() {
-//     return (
-//         <BrowserRouter>
-//             <Routes>
-             
-//                 <Route path="/" element={<Home />} ></Route>
-//                 <Route path="/informacao" element={<Info />} ></Route>
-//                 <Route path="/lista" element={<Lista/>} ></Route>
-//                 <Route path="/fazerdenuncia" element={<Denuncia/>} ></Route>
-               
-//             </Routes>
-//     </BrowserRouter>
-//     )
-// }
-
-// export default AppRoutes
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../Pages/Home/index.jsx';
@@ -40,7 +18,7 @@ function AppRoutes() {
                 <Route path="/informacao" element={<Info />} />
                 <Route path="/lista" element={<Lista />} />
                 <Route path="/fazerdenuncia" element={<Denuncia />} />
-                <Route path="/Register" element={<Register />} />
+                <Route path="/register" element={<Register />} />
                 <Route path="/sobre" element={<Sobre />} />
                 
             </Routes>
